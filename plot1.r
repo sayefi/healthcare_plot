9c9d5a26282fca679e8d2182ecb25111148551ce
sayef <sayefi@gmail.com>
2017-09-13 23:22:17 -0400

    Submitted

--- plot1.r
+++ plot1.r
@@ -23,7 +23,8 @@ colv<-colv+1
 myColors<-topo.colors(2,alpha=.4)
 vColors<-myColors[colv[seq(1,length(colv),1)]]
 
-
+par(mfrow=c(1,1))
+par(mar=c(4,5,5,1),cex.main=2)
 
 plot(log10(pDataNy$Average.Total.Payments),log10(pDataNy$Average.Covered.Charges),
      col=vColors,pch=19,bg="gray",
@@ -52,12 +53,13 @@ dev.off()
 dDef<-unique(pData$DRG.Definition)
 states<-unique(pData$Provider.State)
 
-r<-length(dDef)
-c<-length(states)
+
 
 # par(mfrow=c(r,c))
+
 par(mfrow=c(2,3))
-par(mar=c(1,1,1,1))
+par(mar=c(4,5,5,1),cex.main=1)
+
 
 for(n in 1:6){
      plotData<-subset(pData,DRG.Definition==dDef[n])
@@ -66,50 +68,34 @@ for(n in 1:6){
                       Average.Total.Payments)
      
      plotData<-group_by(plotData,Provider.State)
-     plotData<-dplyr::summarise_all(pData,mean)
+     plotData<-dplyr::summarise_all(plotData,mean)
      
+     # rownames(plotData)<-plotData$Provider.State
+     
+     plotDataMatrix<-as.matrix(t(plotData[2:3]))
+     colnames(plotDataMatrix)<-plotData$Provider.State
      
      
      #plotData<-subset(pData,Provider.State==states[n])
      
-     barplot(plotData[3:4],plotData$Provider.State)
+     barplot(plotDataMatrix,beside=TRUE,col=topo.colors(2,alpha=.80),
+             ylim=c(0,85000),main=dDef[n],ylab="Amount in $",xlab="State")
      
-     # colv<-plotData$Average.Total.Payments>plotData$Average.Covered.Charges
-     # 
-     # colv<-colv+1
-     # 
-     # # myColors<-topo.colors(2,alpha=.4)
-     # vColors<-myColors[colv[seq(1,length(colv),1)]]
-     # 
-     # plot(log10(plotData$Average.Total.Payments),
-     #      log10(plotData$Average.Covered.Charges),
-     #      pch=19, col=vColors,main=states[n])
+     abline(h=mean(plotData$Average.Covered.Charges),
+            col = topo.colors(2,alpha=.80)[1], lwd=2,lty = 3)
      
+     abline(h=mean(plotData$Average.Total.Payments),
+            col ="gray", lwd=2,lty = 3)
      
+     # abline(h=mean(plotData$Average.Covered.Charges),col = "gray", lwd=2,lty = 3)
      
-     # for(nCol in 1:c){
-     #      plotData<-subset(pData,DRG.Definition==dDef[nRow])
-     #      plotData<-subset(plotData,Provider.State==states[nCol])
-     #      
-     #      plot(log10(plotData$Average.Total.Payments),log10(plotData$Average.Covered.Charges),
-     #           pch=19)
-     # }
+     if(n==1){
+          legend("topleft", c("Average Covered Charges","Average Payments"), cex=1, 
+                 bty="n", fill=topo.colors(2,alpha=.80))
+     }
+     
 }
 
-dim(pData)
 
-pData<-select(pData, DRG.Definition, Provider.State,Average.Covered.Charges,
-              Average.Total.Payments  )
-# pData<-select(pData,)
-
-plot(log10(pData$Average.Total.Payments),log10(pData$Average.Covered.Charges),
-     col=pData$DRG.Definition,pch=19)
-
-plot(log10(pData$Average.Total.Payments),log10(pData$Average.Covered.Charges),
-     col=pData$Provider.State,pch=19)
-# plot(pData$Average.Total.Payments,pData$Average.Covered.Charges)
-
-pairs(pData[1:4], pch = 19)
-
-dev.copy(pdf,"plot12.pdf",paper="A4r",width=640,height=480)
+dev.copy(pdf,"plot2.pdf",paper="A4r",width=640,height=480)
 dev.off()
